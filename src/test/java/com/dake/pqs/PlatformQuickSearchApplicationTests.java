c06c5dc84db4ad5c90369c9333d92111f176ac42
Modular-Coder <43522750+Modular-Coder@users.noreply.github.com>
2020-03-01 19:32:01 -0600

    Spring starter initial commit

---
+++ src/test/java/com/dake/pqs/PlatformQuickSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dake.pqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformQuickSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
