1895bfa6564fc9d7af3d3ecedf7bb4d01863dfb2
Modular-Coder <43522750+Modular-Coder@users.noreply.github.com>
2020-03-05 18:17:07 -0600

    Convert project to use Kotlin

--- README.md
+++
@@ -1,2 +0,0 @@
-# platform-quick-search
-Quickly search through popular platforms based off historical data
---
+++ src/main/kotlin/com/dake/pqs/PlatformQuickSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.dake.pqs
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlatformQuickSearchApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlatformQuickSearchApplication>(*args)
+}
